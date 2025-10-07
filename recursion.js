a8362f7cbe3d91bf18e607484e4c595bebc1328a
timoteostifft <timoteostifft@gmail.com>
2025-10-07 13:41:39 -0300

    feat: trace steps

--- recursion.js
+++ recursion.js
@@ -1,27 +1,31 @@
 let best = Infinity;
+let steps = "";
 
 function print(value) {
   console.log(value);
 }
 
-function count(n, sum = 0) {
+function count(n, sum = 0, path = "") {
   if (n == 1) {
     if (sum < best) {
       best = sum;
+      steps = path.trim();
     }
 
     return;
   }
 
   if (n % 2 == 0) {
-    count(n / 2, sum + 1);
+    count(n / 2, sum + 1, path + " /2");
   }
 
   if (n % 3 == 0) {
-    count(n / 3, sum + 1);
+    count(n / 3, sum + 1, path + " /3");
   }
 
-  count(n - 1, sum + 1);
+  count(n - 1, sum + 1, path + " -1");
 }
 
-count(parseInt(process.argv[2])), print(best);
+const n = parseInt(process.argv[2]);
+count(n);
+print(`${best} operações: ${steps}`);
